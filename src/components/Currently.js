c44dc7d6f051523357ec36d07537b3ec80a3f79a
Amin Mushehid <amin.mushehid@chasacademy.se>
2019-03-19 12:25:22 +0100

    Move state up to <App /> component.
    Move state from Currently component to App component and turn Currently into a stateless component. Add if-statement to prevent unnecessary API calls to weather API.

--- src/components/Currently.js
+++ src/components/Currently.js
@@ -1,53 +1,13 @@
-import React, { Component } from 'react';
-import withWeather from '../hoc/withWeather.js';
-import withGeolocation from '../hoc/withGeolocation.js';
+import React from 'react';
 
 // Component for current weather
-class Currently extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tempUnits: "si"
-    };
-    this.fetchLocation = this.fetchLocation.bind(this);
-    this.getWeather = this.getWeather.bind(this);
-  }
+const Currently = props => {
 
-  async fetchLocation() {
-    const { getLocation } = this.props;
-
-    const location = await getLocation();
-    return location;
-  }
-
-  async getWeather(location) {
-    const { fetchWeather } = this.props;
-    const { tempUnits } = this.state; // Set default temperature unit to si (celsius)
-
-    const weather = await fetchWeather(location, tempUnits);
-    console.log(weather);
-    return weather;
-  }
-
-
-  componentDidMount() {
-    // Get the location and set it in State
-    this.fetchLocation()
-      .then(res => {
-        this.setState({ location: res });
-      });
-  }
-
-  componentDidUpdate() {
-    this.getWeather(this.state.location);
-  }
-
-  render() {
     // If location is set in state, render location. Otherwise render loading indicator.
-    let showLocation = this.state.location ?
+    let showLocation = props.location ?
       (<ul>
-        <li>{this.state.location.latitude}</li>
-        <li>{this.state.location.longitude}</li>
+        <li>{props.location.latitude}</li>
+        <li>{props.location.longitude}</li>
       </ul>) : (
         <p>Loading...</p>
       );
@@ -57,8 +17,6 @@ class Currently extends Component {
         {showLocation}
       </div>
     );
-  };
 }
 
-
-export default withGeolocation(withWeather(Currently));
+export default Currently;
