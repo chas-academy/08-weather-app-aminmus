692199e47f08491374263c410e68b835f65b2837
Amin Mushehid <amin.mushehid@gmail.com>
2019-08-17 17:44:29 +0200

    Refactor withWeather HOC to async/await.

--- src/hoc/withWeather.jsx
+++ src/hoc/withWeather.jsx
@@ -4,9 +4,12 @@ import React from 'react';
 // High Order Component for fetching weather data
 // `https://api.darksky.net/forecast/${key}/${latitude},${longitude}?${optionalParams}`
 const withWeather = (WrappedComponent) => {
-  const fetchWeather = (location, units) => fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/44ac77a2c728fd9b159d612f3f3b227f/${location.latitude},${location.longitude}?units=${units}`)
-    .then((res) => res.json())
-    .then((res) => res);
+  const fetchWeather = async (location, units) => {
+    const url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/44ac77a2c728fd9b159d612f3f3b227f/${location.latitude},${location.longitude}?units=${units}`;
+    const weather = await fetch(url);
+
+    return weather.json();
+  };
   return (props) => <WrappedComponent fetchWeather={fetchWeather} {...props} />;
 };
 
