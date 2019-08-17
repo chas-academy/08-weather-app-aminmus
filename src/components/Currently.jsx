8ae571ef8bd1df70a74d81b642703ae6540bef1f
Amin Mushehid <amin.mushehid@gmail.com>
2019-08-17 23:43:05 +0200

    Make draft structure of <Currently />. Add font-awesome.

--- src/components/Currently.jsx
+++ src/components/Currently.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+
+
+const Wrapper = styled.div`
+  background-color: lightblue;
+`;
+const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+`;
+
 
 // Component for current weather
+// TODO: Replace hardcoded information with data from props
 const Currently = ({ location }) => {
   // If location is set in state, render location. Otherwise render loading indicator.
   const showLocation = location
     ? (
-      <ul>
-        <li>{location.latitude}</li>
-        <li>{location.longitude}</li>
-      </ul>
+      <>
+        <Wrapper>
+          <Info>
+            <FontAwesomeIcon icon={faSun} />
+            <StyledUl>
+              <li>16:00</li>
+              <li>22°C</li>
+              <li>humidity: 60%</li>
+              <li>wind speed: 11km/h</li>
+              <li>sunrise: 07:00</li>
+            </StyledUl>
+          </Info>
+        </Wrapper>
+
+
+        <ul>
+          <li>{location.latitude}</li>
+          <li>{location.longitude}</li>
+        </ul>
+      </>
     ) : (
       <p>Loading...</p>
     );
