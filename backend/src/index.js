13ca86801a302985db26ff576d129b8ccd0997fd
Amin Mushehid <amin.mushehid@gmail.com>
2019-08-29 19:29:09 +0200

    Add NodeJS API route for darksky API requests.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,5 @@ const app = express();
 
 app.use('/api', require('./routes/routes'));
 
-app.get('/', function (req, res) {
-  res.send('hello world')
-});
-
 app.listen(8080, () => console.log('Backend is running'));
 
--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -2,11 +2,17 @@ const router = require('express').Router();
 const fetch = require('node-fetch');
 
 router.get('/weather/:latitude/:longitude/:units', async (req, res) => {
-  const { latitude, longitude, units } = req.params;
+  try {
+    const { latitude, longitude, units } = req.params;
 
-  const weather = await fetch(`https://api.darksky.net/forecast/44ac77a2c728fd9b159d612f3f3b227f/${latitude},${longitude}?units=${units}`);
+    const weatherRequest = await fetch(`https://api.darksky.net/forecast/44ac77a2c728fd9b159d612f3f3b227f/${latitude},${longitude}?units=${units}`);
 
-  res.send(await weather.json());
+    const weather = await weatherRequest.json()
+
+    return res.status(weatherRequest.status).send(weather);
+  } catch (error) {
+    return res.status(500).send({ error: { message: error.message } });
+  }
 });
 
 module.exports = router;
